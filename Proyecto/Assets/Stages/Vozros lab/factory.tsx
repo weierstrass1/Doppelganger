c1970e9415156127e0c77b7eff2745bdb3776371
weierstrass1 <pndiaz1@uc.cl>
2019-09-17 00:50:56 -0300

    asd

--- Proyecto/Assets/Stages/Vozros lab/factory.tsx
+++ Proyecto/Assets/Stages/Vozros lab/factory.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="factory" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
- <image source="../../../../Nox/Sector 1 FG/FGSector1v12trans.png" width="1025" height="1025"/>
+<tileset version="1.2" tiledversion="1.2.4" name="piso lab" tilewidth="16" tileheight="16" tilecount="210" columns="21">
+ <image source="../../../../../../../Desktop/piso lab.png" width="336" height="160"/>
 </tileset>
