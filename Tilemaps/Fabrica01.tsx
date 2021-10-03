2e2dbab703944cb2469800259d66df9d0f4f8a71
Luis Medina <lmedinar1503@alumno.ipn.mx>
2021-10-02 19:52:28 -0500

    Prueba desde git

---
+++ Tilemaps/Fabrica01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Fabrica01" tilewidth="16" tileheight="16" tilecount="3264" columns="51" backgroundcolor="#1e101c">
+ <image source="../Nox/Sector 1 FG/FGSector1v21.png" width="816" height="1024"/>
+</tileset>
